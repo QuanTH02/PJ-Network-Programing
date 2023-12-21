31be26cb54a71ab87bbffd33e9a945f56e17038d
quanth02 <anhquankaka113@gmail.com>
2023-12-22 01:38:07 +0700

    General

--- client.py
+++ client.py
@@ -6,20 +6,6 @@ ADDR = (IP, PORT)
 FORMAT = "utf-8"
 SIZE = 1024
 
-def login(client):
-    username = input("Enter username: ")
-    password = input("Enter password: ")
-    client.send(f"LOGIN\n{username}\n{password}".encode(FORMAT))
-    response = client.recv(SIZE).decode(FORMAT)
-    print(f"{response}")
-
-    if response.startswith("1030"):
-        print("Login successful!")
-        return {"username": username}
-    else:
-        print("Login failed. Please try again.")
-        return None
-
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(ADDR)
