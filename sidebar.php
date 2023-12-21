d4ed9da90b6594ae57427fe987621f20f8a2e685
quanth02 <anhquankaka113@gmail.com>
2023-12-22 01:01:20 +0700

    FE_v1

---
+++ sidebar.php
@@ -0,0 +1,29 @@
+<h2 class="mt-5 pb-4" id="name-app" style="text-align: center; font-weight: 800;">Cloudy</h2>
+<div class="sidebar-teams-noti">
+    <ul class="pl-0">
+        <a href="#">
+            <li style="background-color: var(--color-bg-sidebar-li);">
+                <i class="fa-solid fa-users" style="font-size: 16px;"></i>
+                <p>Teams</p>
+            </li>
+        </a>
+
+        <a href="#">
+            <li>
+                <i class="fa-regular fa-bell pl-1" style="margin-right: 12px; padding-right: 0px;"></i>
+                <p>Notifications</p>
+            </li>
+        </a>
+    </ul>
+</div>
+
+<div class="sidebar-img-username" style="position: absolute; bottom: 20px; width: 180px;">
+    <div>
+        <img src="img/vebinh1.jpg" alt="username" style="display: inline-block;">
+        <p class="ml-2 mb-0"
+            style="display: inline-block; max-width: 150px; word-wrap: break-word; vertical-align: middle;">
+            username</p>
+
+    </div>
+    <a href="#"><i class="fa-solid fa-arrow-right-from-bracket" style="float: right; display: block;"></i></a>
+</div>
