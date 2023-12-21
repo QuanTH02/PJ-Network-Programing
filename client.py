36a832eded53e34795dea0a66205e953a615ddbe
Nguyen Hai Nam <abckaiser0@gmail.com>
2023-12-22 02:05:52 +0700

    update

--- client.py
+++ client.py
@@ -1,87 +1,11 @@
 import socket
 import os
 
-IP = socket.gethostbyname(socket.gethostname())
+IP = "127.0.0.1"
 PORT = 5000
 ADDR = (IP, PORT)
 FORMAT = "utf-8"
 SIZE = 1024
-
-
-def display_login_menu():
-    print("1. Login")
-    print("2. Signup")
-    print("3. Exit")
-    choice = input("Choose an option (1/2/3): ")
-    return choice
-
-
-def display_main_menu():
-    print("1. Show My Teams")
-    print("2. List Files")
-    print("3. Upload File")
-    print("4. Make Directory")
-    print("5. Delete File")
-    print("6. Create Team")
-    print("7. Logout")
-    choice = input("Choose an option (1-7): ")
-    return choice
-
-
-def login(client):
-    username = input("Enter username: ")
-    password = input("Enter password: ")
-    client.send(f"LOGIN\n{username}\n{password}".encode(FORMAT))
-    response = client.recv(SIZE).decode(FORMAT)
-    print(f"{response}")
-
-    if response.startswith("1030"):
-        print("Login successful!")
-        return {"username": username}
-    else:
-        print("Login failed. Please try again.")
-        return None
-
-
-def signup(client):
-    username = input("Enter username: ")
-    password = input("Enter password: ")
-    name = input("Enter your name: ")
-    client.send(f"SIGNUP\n{username}\n{password}\n{name}".encode(FORMAT))
-    response = client.recv(SIZE).decode(FORMAT)
-    print(f"{response}")
-    if response.startswith("1010"):
-        print("Signup successful!")
-    else:
-        print("Signup failed. Please try again.")
-
-
-def show_my_teams(client, username):
-    client.send(f"SHOW_MY_TEAMS\n{username}".encode(FORMAT))
-    response = client.recv(SIZE).decode(FORMAT)
-    print(response)
-
-
-def list_files(client, path):
-    client.send(f"LIST\n{path}".encode(FORMAT))
-    response = client.recv(SIZE).decode(FORMAT)
-    print(response)
-
-
-def upload_file(client, path):
-    with open(path, "r") as f:
-        text = f.read()
-
-    filename = os.path.basename(path)
-    client.send(f"UPLOAD\n{filename}\n{text}".encode(FORMAT))
-    response = client.recv(SIZE).decode(FORMAT)
-    print(response)
-
-
-def make_directory(client, dir_name):
-    client.send(f"MKDIR\n{dir_name}".encode(FORMAT))
-    response = client.recv(SIZE).decode(FORMAT)
-    print(response)
 
 
 def delete_file(client, file_path):
@@ -102,47 +26,19 @@ def main():
     data = client.recv(SIZE).decode(FORMAT)
     print(f"{data}")
 
-    active_session = None
-
     while True:
-        if active_session is None:
-            choice = display_login_menu()
+        data = input("> ")
+        if len(data) == 0:
+            break
+        data = data.split(",")
+        cmd = data[0]
 
-            if choice == "1":
-                active_session = login(client)
-            elif choice == "2":
-                signup(client)
-            elif choice == "3":
-                client.send("LOGOUT".encode(FORMAT))
-                break
-            else:
-                print("Invalid choice. Please try again.")
-
+        if cmd == "CREATE_TEAM":
+            create_team(client, data[1], data[2])
+        elif cmd == "DELETE_FILE":
+            delete_file(client, data[1])
         else:
-            choice = display_main_menu()
-
-            if choice == "1":
-                show_my_teams(client, active_session["username"])
-            elif choice == "2":
-                path = input("Enter directory path: ")
-                list_files(client, path)
-            elif choice == "3":
-                path = input("Enter file path: ")
-                upload_file(client, path)
-            elif choice == "4":
-                dir_name = input("Enter directory name: ")
-                make_directory(client, dir_name)
-            elif choice == "5":
-                file_path = input("Enter file path: ")
-                delete_file(client, file_path)
-            elif choice == "6":
-                team_name = input("Enter team name: ")
-                create_team(client, team_name, active_session["username"])
-            elif choice == "7":
-                client.send("LOGOUT".encode(FORMAT))
-                break
-            else:
-                print("Invalid choice. Please try again.")
+            print("Unknown command.")
 
     print("Disconnected from the server.")
     client.close()
--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import random
 import string
 import bcrypt
 
-IP = socket.gethostbyname(socket.gethostname())
+IP = "127.0.0.1"
 PORT = 5000
 ADDR = (IP, PORT)
 SIZE = 1024
@@ -18,6 +18,23 @@ DB_CONNECTIONS = {}
 DB_CONNECTIONS_LOCK = threading.Lock()
 ACTIVE_SESSIONS = {}
 ACTIVE_SESSIONS_LOCK = threading.Lock()
+
+
+def random_team_code(cursor):
+    characters = string.ascii_letters + string.digits
+    code = "".join(random.choice(characters) for _ in range(6))
+    cursor.execute("Select team_code from team")
+    result = cursor.fetchall()
+    while code in result:
+        characters = string.ascii_letters + string.digits
+        code = "".join(random.choice(characters) for _ in range(6))
+    return code
+
+
+def get_account_id(username, cursor):
+    cursor.execute("SELECT account_id FROM Account WHERE account=?", (username,))
+    result = cursor.fetchone()
+    return result[0] if result else None
 
 
 def get_database_connection():
@@ -37,125 +54,6 @@ def get_database_connection():
 
 def close_connection(dbconn):
     dbconn.close()
-
-
-def random_team_code(cursor):
-    characters = string.ascii_letters + string.digits
-    code = "".join(random.choice(characters) for _ in range(6))
-    cursor.execute("Select team_code from team")
-    result = cursor.fetchall()
-    while code in result:
-        characters = string.ascii_letters + string.digits
-        code = "".join(random.choice(characters) for _ in range(6))
-    return code
-
-
-def is_valid_password(password):
-    if len(password) < 8:
-        return False
-    if not re.search(r"[A-Z]", password):
-        return False
-    if not re.search(r"[a-z]", password):
-        return False
-    if not re.search(r"\d", password):
-        return False
-    return True
-
-
-def get_account_id(username, cursor):
-    cursor.execute("SELECT account_id FROM Account WHERE account=?", (username,))
-    result = cursor.fetchone()
-    return result[0] if result else None
-
-
-def login(conn, data, cursor, addr):
-    username, password = data[1], data[2]
-
-    if username == "" or password == "":
-        send_data = "2011"
-    else:
-        with DB_CONNECTIONS_LOCK:
-            cursor.execute("SELECT password FROM Account WHERE account=?", (username,))
-            result = cursor.fetchone()
-
-        if result and bcrypt.checkpw(password.encode("utf-8"), result[0]):
-            account_id = get_account_id(username, cursor)
-            with ACTIVE_SESSIONS_LOCK:
-                session_key = f"{addr[0]}:{addr[1]}:{account_id}"
-                ACTIVE_SESSIONS[session_key] = {
-                    "account_id": account_id,
-                    "username": username,
-                }
-            send_data = "1030"
-        else:
-            send_data = "2032"
-
-    conn.send(send_data.encode(FORMAT))
-    if "account_id" in locals():
-        return ACTIVE_SESSIONS.get(f"{addr[0]}:{addr[1]}:{account_id}")
-    else:
-        return None
-
-
-def register(conn, data, cursor, dbconn):
-    username, password, name = data[1], data[2], data[3]
-    if username == "" or password == "" or name == "":
-        send_data = "2011"
-    elif is_valid_password(password) is False:
-        send_data = "2012"
-    else:
-        with DB_CONNECTIONS_LOCK:
-            cursor.execute("SELECT account FROM Account WHERE account = ?", (username,))
-            result = cursor.fetchone()
-
-        if result:
-            send_data = "2013"
-        else:
-            send_data = "1010"
-            hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-            cursor.execute(
-                "INSERT INTO Account (account, password, name) VALUES (?, ?, ?)",
-                (username, hashed_password, name),
-            )
-            dbconn.commit()
-
-    conn.send(send_data.encode(FORMAT))
-
-
-def dir_information(conn, data):
-    dir = data[1]
-    path = os.path.join(SERVER_DATA_PATH, dir)
-    files = os.listdir(path)
-    send_data = "1170"
-
-    if len(files) == 0:
-        send_data += "The server directory is empty"
-    else:
-        send_data += "\n".join(f for f in files)
-    conn.send(send_data.encode(FORMAT))
-
-
-def upload_file(conn, data):
-    name, text = data[1], data[2]
-    filepath = os.path.join(SERVER_DATA_PATH, name)
-    with open(filepath, "w") as f:
-        f.write(text)
-
-    send_data = "1180"
-    conn.send(send_data.encode(FORMAT))
-
-
-def make_directory(conn, data):
-    dir_path = data[1]
-    full_path = os.path.join(SERVER_DATA_PATH, dir_path)
-
-    try:
-        os.makedirs(full_path)
-        send_data = "1240"
-    except FileExistsError:
-        send_data = "2042"
-
-    conn.send(send_data.encode(FORMAT))
 
 
 def delete_file(conn, data):
@@ -192,27 +90,6 @@ def create_team(conn, data, cursor, dbconn):
     conn.send(send_data.encode(FORMAT))
 
 
-def show_my_teams(conn, data, cursor):
-    username = data[1]
-    account_id = get_account_id(username, cursor)
-
-    if account_id is not None:
-        cursor.execute(
-            "SELECT team_name FROM team WHERE leader_id = ? OR team_id IN (SELECT team_id FROM team_member WHERE member_id = ?)",
-            (account_id, account_id),
-        )
-        result = cursor.fetchall()
-        if result:
-            teams = [row[0] for row in result]
-            send_data = "1260\n" + "\n".join(teams)
-        else:
-            send_data = "1261\nYou are not a member of any team."
-    else:
-        send_data = "2011"
-
-    conn.send(send_data.encode(FORMAT))
-
-
 def handle_client(conn, addr):
     with get_database_connection() as dbconn:
         cursor = dbconn.cursor()
@@ -220,40 +97,18 @@ def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
     conn.send("OK\nWelcome to the File Server.".encode(FORMAT))
 
-    active_session = None
-
     while True:
         data = conn.recv(SIZE).decode(FORMAT)
+        if len(data) == 0:
+            break
         data = data.split("\n")
         cmd = data[0]
-
-        if cmd == "LOGIN":
-            active_session = login(conn, data, cursor, addr)
-        elif cmd == "LOGOUT":
-            active_session = None
-            break
-        elif cmd == "SIGNUP":
-            register(conn, data, cursor, dbconn)
-        elif active_session is not None:
-            account_id = active_session["account_id"]
-            username = active_session["username"]
-            if cmd == "LIST":
-                dir_information(conn, data)
-            elif cmd == "UPLOAD":
-                upload_file(conn, data)
-            elif cmd == "DELETE":
-                delete_file(conn, data)
-            elif cmd == "MKDIR":
-                make_directory(conn, data)
-            elif cmd == "CREATE_TEAM":
-                create_team(conn, data, cursor, dbconn)
-            elif cmd == "SHOW_MY_TEAMS":
-                show_my_teams(conn, data, cursor)
-            else:
-                conn.send("4040".encode(FORMAT))
-
-        elif len(cmd) == 0:
-            break
+        if cmd == "DELETE":
+            delete_file(conn, data)
+        elif cmd == "CREATE_TEAM":
+            create_team(conn, data, cursor, dbconn)
+        else:
+            conn.send("3000".encode(FORMAT))
 
     print(f"[DISCONNECTED] {addr} disconnected")
     conn.close()
