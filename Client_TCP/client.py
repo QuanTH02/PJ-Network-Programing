c1d3ddf9b28dd1953443afcd8540fdf97bc5d2f8
quanth02 <anhquankaka113@gmail.com>
2024-01-10 00:49:20 +0700

    first commit

---
+++ Client_TCP/client.py
@@ -0,0 +1,372 @@
+import random
+import socket
+import os
+import string
+import sys
+
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 5000
+ADDR = (IP, PORT)
+FORMAT = "utf-8"
+SIZE = 1024
+FILE_BLOCK_SIZE = 131072
+CLIENT_DATA_PATH = "client_data"
+
+def responseFromServer(client):
+    # print("Go to responseFromServer")
+    requests = client.recv(SIZE).decode(FORMAT)
+
+    # Sửa
+    responses = [request for request in requests.split('\r\n') if request]
+
+    for response in responses:
+        # Đăng ký và quản lý tài khoản
+        if response.startswith("1010"):
+            print("Đăng ký thành công")
+
+        # LOGIN
+        elif response.startswith("1030"):
+            print("Đăng nhập thành công")
+        elif response.startswith("2011"):
+            print("Chưa nhập đủ các trường thông tin")
+        elif response.startswith("2031"):
+            print("Tài khoản không tồn tại")
+        elif response.startswith("2032"):
+            print("Mật khẩu không đúng")
+        
+        # if response.startswith("2011"):
+        #     print("Chưa nhập đủ các trường thông tin")
+        elif response.startswith("2012"):
+            print("Trường thông tin nhập chưa đúng định dạng")
+        elif response.startswith("2013"):
+            print("Tài khoản đã tồn tại")
+        elif response.startswith("1020"):
+            print("Đổi mật khẩu thành công")
+        elif response.startswith("2021"):
+            print("2 mật khẩu trùng nhau")
+        elif response.startswith("2022"):
+            print("Mật khẩu mới nhập lại không trùng khớp")
+        elif response.startswith("2023"):
+            print("Mật khẩu mới không đúng định dạng")
+        elif response.startswith("2024"):
+            print("Nhập mật khẩu cũ sai") 
+
+        # SHOW_MY_TEAMS
+        elif response.startswith("1050"):
+            print("Show my teams successfully")
+            lines = response.split("\n")
+            result = "\n".join(lines[1:])
+            print(result)
+        elif response.startswith("1051"):
+            print("Chưa tham gia nhóm nào")
+
+        # GET_MEMBER
+        elif response.startswith("1100"):
+            print("Show team member successfully")
+            lines = response.split("\n")
+            result = "\n".join(lines[1:])
+            print(result)
+        elif response.startswith("2101"):
+            print("Account không nằm trong team")
+
+        # DOWNLOAD
+        elif response.startswith("2182"):
+            print("File không tồn tại")
+
+        # CREATE_FOLDER
+        elif response.startswith("1250"):
+            print("Create folder successfully")
+        elif response.startswith("2251"):
+            print("Thư mục đã tồn tại")
+        elif response.startswith("2252"):
+            print("Tên thư mục không hợp lệ")
+
+        # RENAME_FOLDER
+        elif response.startswith("1260"):
+            print("Rename folder successfully")
+        elif response.startswith("2261"):
+            print("Folder không tồn tại")
+        elif response.startswith("2204"):
+            print("Bạn không có quyền")
+
+        # DELETE_FOLDER
+        elif response.startswith("1270"):
+            print("Delete folder successfully")
+
+        # COPY_FOLDER
+        elif response.startswith("1280"):
+            print("Copy folder successfully") 
+        elif response.startswith("2281"):
+            print("Đã tồn tại folder ở thư mục đích")
+
+        # MOVE_FOLDER
+        elif response.startswith("1290"):
+            print("Move folder successfully")
+             
+        elif response.startswith("1190"):
+            print("Upload file successfully")
+        elif response.startswith("2181"):
+            print("File đã tồn tại ở thư mục đích")
+        elif response.startswith("1200"):
+            print("Download file successfully")
+        elif response.startswith("1060"):
+            print("Join team successfully")
+        elif response.startswith("2061"):
+            print("Nhập sai Team code")
+        elif response.startswith("2062"):
+            print("Đã tham gia nhóm rồi")
+        elif response.startswith("1040"):
+            print("Create team successfully")
+        elif response.startswith("2041"):
+            print("Tên nhóm đã tồn tại")
+        elif response.startswith("2042"):
+            print("Tên nhóm không tồn tại")
+
+        elif response.startswith("3000"):
+            print("Thông điệp không xác định")
+        elif response.startswith("3001"):
+            print("Thông điệp không đúng định dạng giao thức")
+        elif response.startswith("2321"):
+            print("Bạn đã đăng nhập rồi")
+        elif response.startswith("2322"):
+            print("Bạn chưa đăng nhập")
+        elif response.startswith("1320"):
+            print("Đăng xuất thành công")
+        # elif response.startswith(""):
+        #     print("")
+            
+        # print(response)
+        # return response
+
+
+def show_my_teams(client):
+    client.send(f"SHOW_MY_TEAMS\r\n".encode(FORMAT))
+    # response = client.recv(SIZE).decode(FORMAT)
+    # return response
+    # responseFromServer(client)
+
+
+def upload_file(client, path, team_name, des_path):
+    filename = os.path.basename(path)
+    file_size = os.path.getsize(path)
+    client.send(f"UPLOAD\n{filename}\n{team_name}\n{des_path}\n{file_size}\r\n".encode(FORMAT))
+    print("To recv")
+    response = client.recv(SIZE).decode(FORMAT)
+    print("To respond")
+    if response == "2181" or response == "3001" or response == "2101" or response == "2042":
+        return
+
+    print("To open")
+    with open(path, "rb") as f:
+        print("Uploading file...")
+        while True:
+            chunk = f.read(FILE_BLOCK_SIZE)
+            if not chunk:
+                break
+            client.send(chunk)
+
+    # response = client.recv(SIZE).decode(FORMAT)
+    # print(response)
+
+def download_file(client, team_name, src_path, des_path):
+    client.send(f"DOWNLOAD\n{team_name}\n{src_path}\r\n".encode(FORMAT))
+    file_path = os.path.join(des_path, os.path.basename(src_path))
+
+    response = client.recv(SIZE).decode(FORMAT)
+    if response == '2042' or response == '2101' or response == '2182':
+        return
+
+    if os.path.exists(file_path):
+        file_name = os.path.basename(src_path)
+        new_file_name = file_name.split('.')[0] + '(1).' + file_name.split('.')[1]
+        file_path = os.path.join(des_path, new_file_name)
+
+    file_size = int(client.recv(SIZE).decode(FORMAT))
+    with open(file_path, "wb") as f:
+        while file_size > 0:
+            chunk = client.recv(FILE_BLOCK_SIZE)
+            f.write(chunk)
+            file_size -= len(chunk)
+
+def show_team_member(client, team_name):
+    print("Show team member")
+    client.send(f"GET_MEMBER\n{team_name}\r\n".encode(FORMAT))
+    # response = client.recv(SIZE).decode(FORMAT)
+    # print(response)
+
+def create_directory(client, team_name, dir_path, dir_name):
+    client.send(f"CREATE_FOLDER\n{team_name}\n{dir_path}\n{dir_name}\r\n".encode(FORMAT))
+    # response = client.recv(SIZE).decode(FORMAT)
+    # print(response)
+
+def rename_directory(client, team_name, dir_path, dir_name, dir_new_name):
+    client.send(f"RENAME_FOLDER\n{team_name}\n{dir_path}\n{dir_name}\n{dir_new_name}\r\n".encode(FORMAT))
+    # response = client.recv(SIZE).decode(FORMAT)
+    # print(response)
+
+def delete_directory(client, team_name, dir_path, dir_name):
+    client.send(f"DELETE_FOLDER\n{team_name}\n{dir_path}\n{dir_name}\r\n".encode(FORMAT))
+    # response = client.recv(SIZE).decode(FORMAT)
+    # print(response)
+
+def copy_directory(client, team_name, src_path, dir_name, des_path):
+    client.send(f"COPY_FOLDER\n{team_name}\n{src_path}\n{dir_name}\n{des_path}\r\n".encode(FORMAT))
+    # response = client.recv(SIZE).decode(FORMAT)
+    # print(response)
+
+def move_directory(client, team_name, src_path, dir_name, des_path):
+    client.send(f"MOVE_FOLDER\n{team_name}\n{src_path}\n{dir_name}\n{des_path}\r\n".encode(FORMAT))
+    # response = client.recv(SIZE).decode(FORMAT)
+    # print(response)
+
+
+####################################################################
+####################################################################
+####################################################################
+def login(client, username, password):
+    client.send(f"LOGIN\n{username}\n{password}\r\n".encode(FORMAT))
+    
+def signup(client, username, password, name):
+    client.send(f"SIGNUP\n{username}\n{password}\n{name}\r\n".encode(FORMAT))
+    # response = client.recv(SIZE).decode(FORMAT)
+    # print(f"{response}")
+
+def create_team(client, team_name):
+    client.send(f"CREATE_TEAM\n{team_name}\r\n".encode(FORMAT))
+    # response = client.recv(SIZE).decode(FORMAT)
+    # print(response)
+
+def join_team(client, team_code):
+    print("To join team")
+    client.send(f"JOIN_TEAM\n{team_code}\r\n".encode(FORMAT))
+    print("Send successful")
+    # response = client.recv(SIZE).decode(FORMAT)
+    # print(response)
+
+def logout(client):
+    client.send(f"LOGOUT\r\n".encode(FORMAT))
+    # response = client.recv(SIZE).decode(FORMAT)
+    # print(f"{response}")
+####################################################################
+####################################################################
+####################################################################
+
+
+def main():
+    global client
+    if len(sys.argv) != 2:
+        print("Usage: python server.py <port>")
+        return
+    IP = socket.gethostbyname(socket.gethostname())
+    PORT = int(sys.argv[1])
+    ADDR = (IP, PORT)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+    data = client.recv(SIZE).decode(FORMAT)
+    print(f"{data}")
+
+    while True:
+        data = input("> ")
+        if len(data) == 0:
+            break
+        data = data.split("|")
+        cmd = data[0]
+
+        if cmd == "LOGIN":
+            if len(data) != 3:
+                print("Usage: LOGIN|<account>|<password>")
+                continue
+            else:
+                login(client, data[1], data[2])
+        elif cmd == "SIGNUP":
+            if len(data) != 4:
+                print("Usage: SIGNUP|<account>|<password>|<name>")
+                continue
+            else:
+                signup(client, data[1], data[2], data[3])
+        elif cmd == "LOGOUT":
+            if len(data) != 1:
+                print("Usage: LOGOUT")
+                continue
+            else:
+                logout(client)
+        elif cmd == "SHOW_MY_TEAMS":
+            if len(data) != 1:
+                print("Usage: SHOW_MY_TEAMS")
+                continue
+            else:
+                show_my_teams(client)
+        elif cmd == "UPLOAD":
+            if len(data) != 4:
+                print("Usage: UPLOAD|<src_path>|<team_name>|<des_path>")
+                continue
+            else:
+                upload_file(client, data[1], data[2], data[3])
+        elif cmd == "DOWNLOAD":
+            if len(data) != 4:
+                print("Usage: DOWNLOAD|<team_name>|<src_path>|<des_path>")
+                continue
+            else:
+                download_file(client, data[1], data[2], data[3])
+        elif cmd == "GET_MEMBER":
+            if len(data) != 2:
+                print("Usage: GET_MEMBER|<team_name>")
+                continue
+            else:
+                show_team_member(client, data[1])
+        elif cmd == "CREATE_FOLDER":
+            if len(data) != 4:
+                print("Usage: CREATE_FOLDER|<team_name>|<des_path>|<name_folder>")
+                continue
+            else:
+                create_directory(client, data[1], data[2], data[3])
+        elif cmd == "RENAME_FOLDER":
+            if len(data) != 5:
+                print("Usage: RENAME_FOLDER|<team_name>|<des_path>|<name_folder>|<new_name_folder>")
+                continue
+            else:
+                rename_directory(client, data[1], data[2], data[3], data[4])
+        elif cmd == "DELETE_FOLDER":
+            if len(data) != 4:
+                print("Usage: DELETE_FOLDER|<team_name>|<dir_path>|<dir_name>")
+                continue
+            else:
+                delete_directory(client, data[1], data[2], data[3])
+        elif cmd == "COPY_FOLDER":
+            if len(data) != 5:
+                print("Usage: COPY_FOLDER|<team_name>|<src_path>|<dir_name>|<des_path>")
+                continue
+            else:
+                copy_directory(client, data[1], data[2], data[3], data[4])
+        elif cmd == "MOVE_FOLDER":
+            if len(data) != 5:
+                print("Usage: MOVE_FOLDER|<team_name>|<src_path>|<dir_name>|<des_path>")
+                continue
+            else:
+                move_directory(client, data[1], data[2], data[3], data[4])
+        ###########################
+        elif cmd == "JOIN_TEAM":
+            join_team(client, data[1])
+        ###########################
+            
+        elif cmd == "CREATE_TEAM":
+            if len(data) != 2:
+                print("Usage: CREATE_TEAM|<team_name>")
+                continue
+            else:
+                create_team(client, data[1])
+        else:
+            send_data = '\n'.join(data) + '\r\n'
+            client.send(send_data.encode(FORMAT))
+            
+        responseFromServer(client)
+
+            
+
+    print("Disconnected from the server.")
+    client.close()
+
+
+if __name__ == "__main__":
+    main()
+
